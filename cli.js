a8d98ff24184d56ba266463ff16cedde9ee5041f
Danylo Dotsenko <dotsenkodanylo@gmail.com>
2020-04-20 15:20:43 -0400

    1.0.0_IMAGE_PADDER: UPDATE ~ Finalized parsing for initial user input, where the input is caught and parsed for whether it points to an image, directory, or neither, in which case, catch the possible error.

--- cli.js
+++ cli.js
@@ -1,24 +1,48 @@
 let fs = require('fs');
 let input = process.argv.slice(2);
 
-
-const checkIfSingleInput = (args) => new Promise((resolve, reject) => {
-    if (args.length > 0) {
-        if (args.length !== 1) {
+const checkIfSingleInput = (input) => new Promise((resolve, reject) => {
+    if (input.length > 0) {
+        if (input.length !== 1) {
             reject(`Multiple arguments detected; please enter a single image directory!`);
         } else {
-            console.log('Thank you!; processing...');
-            resolve(args[0]);
+            resolve(input[0]);
         }
-
     } else {
         reject('No input directory detected; Please enter a directory!');
     }
 });
 
-checkIfSingleInput(input).then(directory => {
-    fs.readdir(directory, (err, files) => {
-        console.log(files);
+const checkIfValidInput = (input) => new Promise((resolve, reject) => {
+    let imageReg = /[\/.](gif|jpg|jpeg|tiff|png)$/i;
+
+    if (imageReg.test(input)) {
+        if (fs.existsSync(input)) {
+            resolve('image');
+        } else {
+            reject('Image does not exist; please try again!');
+        }
+    } else {
+        try {
+            fs.statSync(input);
+            resolve('directory');
+        } catch(e) {
+            reject('Directory does not exist; please try again!');
+        }
+    }
+});
+
+checkIfSingleInput(input).then(path => {
+    checkIfValidInput(path.toString()).then(type => {
+        // Check the returned parameter from the promise as to whether the
+        // processed input is an image, or directory. If the preceeding,
+        // perform the required edit. If the latter, scoop through the
+        // entire directory, take all the files, and output them; we want to
+        // show all the files first, as well as prompt the user for
+        // confirmation as to whether all the caught files require editing. 
+        console.log(type);
+    }).catch(e => {
+        console.log(e);
     });
 })
     .catch(error => {
